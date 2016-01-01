ee38972ea816786b0c44e1db971cc92adabda835
Cameron <cameron.p.buckingham@gmail.com>
2016-01-01 13:16:16 -0700

    cart total price to represent purchasing process

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         def cart_count
-         $redis.scard "cart#{id}"
-       end
+def cart_count
+  $redis.scard "cart#{id}"
+end
+
+def cart_total_price
+  total_price = 0
+  get_cart_movies.each { |movie| total_price+= movie.price }
+  total_price
+end
+
+def get_cart_movies
+  cart_ids = $redis.smembers "cart#{id}"
+  Movie.find(cart_ids)
+end
+
 end
