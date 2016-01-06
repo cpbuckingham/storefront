a20da8dfea2cd061bb19d567e5d6835ec5fd9b5a
Cameron <cameron.p.buckingham@gmail.com>
2016-01-05 17:36:51 -0700

    seed data updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation();
+  var url = data.url + "&output=embed";
+  window.location.replace(url);
+});
